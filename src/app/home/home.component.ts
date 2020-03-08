b420840b1a0578c4a8fb5d3075d3f9e77cb77da9
githumbi <githumbi74@gmail.com>
2020-03-08 14:38:50 +0300

    adding click event

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  clickCounter: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  countClick() {
+    this.clickCounter += 1;
+  }
 }
